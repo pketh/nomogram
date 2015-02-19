e249643e2cf723c38e757f6588b7319bd7c3a89c
Pirijan ☃ <pirijan@gmail.com>
2015-02-18 21:31:42 -0500

    model fix

--- models.rb
+++ models.rb
@@ -18,6 +18,7 @@ end
 class Post
   include DataMapper::Resource
   belongs_to :project
+  property :id, Serial
   # properties ...
   has n, :comments
 end
@@ -25,6 +26,7 @@ end
 class Comment
   include DataMapper::Resource
   belongs_to :post
+  property :id, Serial
   # properties ...
 end
 
